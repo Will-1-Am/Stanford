40d98f1b3297c24dc7733d83008c85bb540a19eb
William Spanfelner <wil-1-am@outlook.com>
2021-10-07 16:04:52 +0300

    Create a text view using an emoji as the content of a face up card; Place the text view in a ZStack container to effectively layer views one top of one another to create a face up card; Create the background and border of the card using RoundedRectangle;

--- MemorizeCS193p/ContentView.swift
+++ MemorizeCS193p/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+            RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+            Text("👻")
+        }
+        .foregroundColor(Color.orange)
+        .padding()
+        .font(Font.largeTitle)
     }
 }
 
