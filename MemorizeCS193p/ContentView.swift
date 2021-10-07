61e6cebeefb667bfe1a6dcd8e3edbb2fd12437dd
William Spanfelner <wil-1-am@outlook.com>
2021-10-07 16:23:14 +0300

    Create a new CardView struct to contain the layered face up card view;

--- MemorizeCS193p/ContentView.swift
+++ MemorizeCS193p/ContentView.swift
@@ -9,16 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        CardView()
+            .foregroundColor(Color.orange)
+            .padding()
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
             RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
             Text("👻")
         }
-        .foregroundColor(Color.orange)
-        .padding()
-        .font(Font.largeTitle)
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
