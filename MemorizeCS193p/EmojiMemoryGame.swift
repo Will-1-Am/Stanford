113793d3b5a7c1b6bf6624f5e16b749bc4412e16
William Spanfelner <wil-1-am@outlook.com>
2021-10-08 21:01:34 +0300

    Add initialiser to MemoryGame which really depends upon the view model to complete the process because only it will know the type of CardContent;

--- MemorizeCS193p/EmojiMemoryGame.swift
+++ MemorizeCS193p/EmojiMemoryGame.swift
@@ -7,13 +7,10 @@
 
 import Foundation
 
-// MARK: - EmojiMemoryGame will be the view model and will express intents to
-// access the model on behalf of the view
-
 class EmojiMemoryGame {
-    // MARK: - An EmojiMemoryGame needs to have cards, and a way to choose those
-    // cards -  model is specified here with CardContent of type String and
-    // marked private since only EmojiMemoryGame should have access to it.
+    // FIXME: - "Class 'EmojiMemoryGame' has no initializers" means there is a
+    // property that doesn't have a way to have a value if the class were to be
+    // instantiated now.
     private var model: MemoryGame<String>
     
     // MARK: - Access to the model
@@ -21,9 +18,7 @@ class EmojiMemoryGame {
         model.cards
     }
     
-    // MARK: - Intents allow a view to "speak" to the model adn request that
-    // actions be performed on the model.  A view would be able to see the model
-    // but, would not be able to change the model without intents.
+    // MARK: - Intents
     func choose(card: MemoryGame<String>.Card) {
         model.choose(card: card)
     }
--- MemorizeCS193p/Model/MemoryGame.swift
+++ MemorizeCS193p/Model/MemoryGame.swift
@@ -9,6 +9,21 @@ import Foundation
 
 struct MemoryGame<CardContent> {
     var cards: Array<Card>
+    
+    // MARK: - A MemoryGame wants to be created for a known number of pairs of
+    // cards and then create an array of cards - the deck in other words - from
+    // which to play the game. numberOfPairsOfCards will be the known number of
+    // pairs of type integer.  cardContentFactory will take a closure of type
+    // integer and return CardContent
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        // MARK: - Create an empty of cards
+        cards = Array<Card>()
+        for pairIndex in 0 ..< numberOfPairsOfCards {
+            // MARK: - What should CardContent be???
+            cards.append(Card(isFaceUp: false, isMatched: false, content: <#T##CardContent#>))
+            cards.append(Card(isFaceUp: false, isMatched: false, content: <#T##CardContent#>))
+        }
+    }
 
     func choose(card: Card) {
         print("User chose card: \(card)")
