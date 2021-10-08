f72ef207b62367a2dc53514f7616af2cef3ba2ad
William Spanfelner <wil-1-am@outlook.com>
2021-10-08 20:16:25 +0300

    Update the MemoryGame declaration to include the CardContent "don't care" or generic type; Add a choose(card:) function that will simply print the value of card to the console at the moment;

--- MemorizeCS193p/Model/MemoryGame.swift
+++ MemorizeCS193p/Model/MemoryGame.swift
@@ -8,18 +8,27 @@
 import Foundation
 
 // MARK: - This model is unaware of the UI and will be somewhat generic in the
-// types of games that can be created with it.
+// types of games that can be created with it. The CardContent type will be
+// specified at the time an instance is created.
 
-struct MemoryGame {
-    // MARK: - A memory game needs some cards
+struct MemoryGame<CardContent> {
     var cards: Array<Card>
     
-    // MARK: - A card is really a entity in an of itself with its own properties
+    // MARK: - A way of choosing a card is required
+    func choose(card: Card) {
+        print("User chose card: \(card)")
+    }
+    
     struct Card {
         var isFaceUp: Bool
         var isMatched: Bool
         // MARK: - The content on the cards in this game will be emojis, but in
         // other games might be something else (e.g. images, integers, shapes).
+        // Since, the actual type is unknown now, but will be known at the time
+        // of the actual creation of the game, the MemoryGame declaration needs
+        // to be updated to indicate this synonymous with Array.  When the
+        // MemoryGame declaration is updated the compiler error "Cannot find
+        // type 'CardContent' in scope" will be silenced.
         var content: CardContent
     }
 }
