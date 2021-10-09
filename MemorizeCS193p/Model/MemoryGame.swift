f6139dfdb643d264bbedcecaaa2e5639e523f41d
William Spanfelner <wil-1-am@outlook.com>
2021-10-09 21:20:40 +0300

    Add default values to the Card struct to cleanup the Card creation code in the initialiser;

--- MemorizeCS193p/Model/MemoryGame.swift
+++ MemorizeCS193p/Model/MemoryGame.swift
@@ -9,20 +9,13 @@ import Foundation
 
 struct MemoryGame<CardContent> {
     var cards: Array<Card>
-    
-    // MARK: - A MemoryGame wants to be created for a known number of pairs of
-    // cards and then create an array of cards - the deck in other words - from
-    // which to play the game. numberOfPairsOfCards will be the known number of
-    // pairs of type integer.  cardContentFactory will take a closure of type
-    // integer and return CardContent
+
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
         cards = Array<Card>()
         for pairIndex in 0 ..< numberOfPairsOfCards {
-            // MARK: - The CardContent type must have to call a function with the
-            // pairIndex to create the two cards in each pair
             let content = cardContentFactory(pairIndex)
-            cards.append(Card(isFaceUp: false, isMatched: false, content: content, id: pairIndex * 2))
-            cards.append(Card(isFaceUp: false, isMatched: false, content: content, id: pairIndex * 2 + 1))
+            cards.append(Card(content: content, id: pairIndex * 2))
+            cards.append(Card(content: content, id: pairIndex * 2 + 1))
         }
     }
 
@@ -31,8 +24,8 @@ struct MemoryGame<CardContent> {
     }
     
     struct Card: Identifiable {
-        var isFaceUp: Bool
-        var isMatched: Bool
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
         var content: CardContent
         var id: Int
     }
