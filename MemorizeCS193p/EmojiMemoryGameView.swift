dc760a4a688424adfcf9c0f0470e187133f8bf66
William Spanfelner <wil-1-am@outlook.com>
2021-10-10 22:18:50 +0300

    Create a control panel of drawing constants to get rid of magic numbers

--- MemorizeCS193p/EmojiMemoryGameView.swift
+++ MemorizeCS193p/EmojiMemoryGameView.swift
@@ -30,16 +30,21 @@ struct CardView: View {
         GeometryReader { geometry in
             ZStack {
                 if card.isFaceUp {
-                    RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                    RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                    RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                     Text(card.content)
                 } else {
-                    RoundedRectangle(cornerRadius: 10.0)
+                    RoundedRectangle(cornerRadius: cornerRadius)
                 }
             }
-            .font(.system(size: .minimum(geometry.size.width, geometry.size.height) * 0.75))
+            .font(.system(size: .minimum(geometry.size.width, geometry.size.height) * fontScaleFactor))
         }
     }
+    
+    // MARK: - Drawing constants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3.0
+    let fontScaleFactor: CGFloat = 0.75
 }
 
 
