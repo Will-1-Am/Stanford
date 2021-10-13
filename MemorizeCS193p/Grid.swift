cb069dbf8080e79688b8d34832f4ee9bb8e8d87b
William Spanfelner <wil-1-am@outlook.com>
2021-10-13 16:09:09 +0300

    Add a ForEach view to the body using items to return a view for each item; Constrain Grid to Identifiable and View for Item and ItemView respectively;

--- MemorizeCS193p/Grid.swift
+++ MemorizeCS193p/Grid.swift
@@ -7,19 +7,25 @@
 
 import SwiftUI
 
-// MARK: - Add the Item & ItemView placeholder types in the Grid definition to clear the
-// error messages and allow these to be defined at the point of creation
-struct Grid<Item, ItemView>: View {
+// MARK: - Constrain Item to Identifiable and ItemView to View - without those
+// constrains the compiler will display the error "No exact matches in call to
+// initializer" - since Item was essentially pulled out of thin air and it has
+// never been made to conform to Identifiable as required by ForEach.
+// Additionally, the compiler also doesn't know anything about ItemView.
+struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     var items: [Item]
     var viewForItem: (Item) -> ItemView
     
-    // MARK: - @escaping will allow the closure to be provided at some later time
     init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
         self.items = items
         self.viewForItem = viewForItem
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // MARK: - Create a ForEach view using the items array and return a
+        // view for each item
+        ForEach(items) { item in
+            self.viewForItem(item)
+        }
     }
 }
