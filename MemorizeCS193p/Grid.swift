9688eb275cb40716da0bf558de26069dc933e8b8
William Spanfelner <wil-1-am@outlook.com>
2021-10-13 13:40:24 +0300

    Add placeholder types to the Grid definition corresponding to the unknown types used to define the Grid struct properties; Add the @escaping to the closure type in the initialiser;

--- MemorizeCS193p/Grid.swift
+++ MemorizeCS193p/Grid.swift
@@ -7,19 +7,14 @@
 
 import SwiftUI
 
-struct Grid: View {
-    // MARK: - The "Cannot find type 'Item' in scope" error is flagging that the
-    // compiler doesn't know what an Item is.  The Item type will have to be
-    // specified at the time an instance of the struct is created - in this case
-    // an initialiser can be used
+// MARK: - Add the Item & ItemView placeholder types in the Grid definition to clear the
+// error messages and allow these to be defined at the point of creation
+struct Grid<Item, ItemView>: View {
     var items: [Item]
-    
-    // MARK: - The "Cannot find type 'ItemView' in scope" error is just saying
-    // that ItemView is also unknown
     var viewForItem: (Item) -> ItemView
     
-    // MARK: - While this is generic struct, an array of cards will be the items here
-    init(_ items: [Item], viewForItem: (Item) -> ItemView) {
+    // MARK: - @escaping will allow the closure to be provided at some later time
+    init(_ items: [Item], viewForItem: @escaping (Item) -> ItemView) {
         self.items = items
         self.viewForItem = viewForItem
     }
