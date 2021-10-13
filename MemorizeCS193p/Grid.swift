ac581dace7b2b2d35534874ea26909f2215d8b3f
William Spanfelner <wil-1-am@outlook.com>
2021-10-13 21:06:18 +0300

    Create extension to Array and update assignment to the index constant;

--- MemorizeCS193p/Grid.swift
+++ MemorizeCS193p/Grid.swift
@@ -28,21 +28,30 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
         }
     }
     
-    // MARK: - Positioning each view requires an index value by GridLayout
-    // GridLayout then uses a computed var itemsize to supply a tuple for the
-    // width and height of a frame the view will be contained within.
-    // GridLayout also provides a "location" method for positioning the center
-    // of the view at a specified index
     func body(for item: Item, in layout: GridLayout) -> some View {
-        let index = self.index(of: item)
+        // MARK: - Here index(of:) is called on item, but the extension to Array can be called directly on the items array
+        let index = items.index(of: item) //self.index(of: item)
         return self.viewForItem(item)
             .frame(width: layout.itemSize.width, height: layout.itemSize.height)
             .position(layout.location(ofItemAt: index))
     }
+// MARK: - Since this method is looping over the array of items and finding a
+// matching index value and a very similar process is happening elsewhere in the
+// code, it makes sense to create an extension to Array to do this task.
+//    func index(of item: Item) -> Int {
+//        for index in 0 ..< items.count {
+//            if items[index].id == item.id {
+//                return index
+//            }
+//        }
+//        return 0 // FIXME: - BOGUS!!
+//    }
+}
 
-    func index(of item: Item) -> Int {
-        for index in 0 ..< items.count {
-            if items[index].id == item.id {
+extension Array where Element: Identifiable {
+    func index(of element: Element) -> Int {
+        for index in 0 ..< self.count {
+            if self[index].id == element.id {
                 return index
             }
         }
