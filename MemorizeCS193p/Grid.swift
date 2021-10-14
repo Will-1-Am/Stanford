10025983e8677b7591a3e2dcbb77d701924ce460
William Spanfelner <wil-1-am@outlook.com>
2021-10-14 13:21:53 +0300

    Rename index(of:) function in extension to Array to better communicate its purpose;

--- MemorizeCS193p/Grid.swift
+++ MemorizeCS193p/Grid.swift
@@ -29,29 +29,17 @@ struct Grid<Item, ItemView>: View where Item: Identifiable, ItemView: View {
     }
     
     func body(for item: Item, in layout: GridLayout) -> some View {
-        // MARK: - Here index(of:) is called on item, but the extension to Array can be called directly on the items array
-        let index = items.index(of: item) //self.index(of: item)
+        let index = items.firstIndex(of: item)
         return self.viewForItem(item)
             .frame(width: layout.itemSize.width, height: layout.itemSize.height)
             .position(layout.location(ofItemAt: index))
     }
-// MARK: - Since this method is looping over the array of items and finding a
-// matching index value and a very similar process is happening elsewhere in the
-// code, it makes sense to create an extension to Array to do this task.
-//    func index(of item: Item) -> Int {
-//        for index in 0 ..< items.count {
-//            if items[index].id == item.id {
-//                return index
-//            }
-//        }
-//        return 0 // FIXME: - BOGUS!!
-//    }
 }
 
 extension Array where Element: Identifiable {
-    func index(of element: Element) -> Int {
+    func firstIndex(of item: Element) -> Int {
         for index in 0 ..< self.count {
-            if self[index].id == element.id {
+            if self[index].id == item.id {
                 return index
             }
         }
