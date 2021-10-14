7253a67e9332d4ccefa4f52cd7f78e307dff7c16
William Spanfelner <wil-1-am@outlook.com>
2021-10-14 19:56:19 +0300

    Constrain CardContent to Equatable protocol in MemoryGame definition;

--- MemorizeCS193p/Model/MemoryGame.swift
+++ MemorizeCS193p/Model/MemoryGame.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct MemoryGame<CardContent> {
+struct MemoryGame<CardContent> where CardContent: Equatable {
     var cards: Array<Card>
 
     init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
@@ -25,15 +25,10 @@ struct MemoryGame<CardContent> {
         if let tappedCardIndex: Int = cards.firstIndex(matching: card),
             !cards[tappedCardIndex].isFaceUp,
             !cards[tappedCardIndex].isMatched {
-            
-            // MARK: - Since we have a card with an index, that isn't already faceUp an isn't matched, lets bind the value of indexOfTheOneAndOnlyFaceUpCard if it
-            // exists to potentialMatchIndex and compare the content values
+
             if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
                 if cards[tappedCardIndex].content == cards[potentialMatchIndex].content {
-                    // MARK: - Attempting to compare the content values of the
-                    // cards will generate an error here ("Binary operator '=='
-                    // cannot be applied to two 'CardContent' operands") since
-                    // CardContent does not conform to Equatable
+
                 }
             }
             print("User chose card: \(card) and tappedCardIndex is: \(tappedCardIndex)")
