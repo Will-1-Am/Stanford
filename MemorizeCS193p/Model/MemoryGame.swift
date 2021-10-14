d584cc4fc18f90b1f68000452861b97f939c4b54
William Spanfelner <wil-1-am@outlook.com>
2021-10-14 17:00:04 +0300

    Replace the call to index(of:) in choose(card:) in the model with a call to firstIndex(matching:);

--- MemorizeCS193p/Model/MemoryGame.swift
+++ MemorizeCS193p/Model/MemoryGame.swift
@@ -19,23 +19,20 @@ struct MemoryGame<CardContent> {
         }
     }
 
-    // MARK: [By default, the properties of a value type can’t be modified from within its instance methods.](https://docs.swift.org/swift-book/LanguageGuide/Methods.html) So, an attempt to
-    // amend the cards array will produce a compiler error "Cannot assign to property: 'self'
-    // is immutable" unless the method is marked mutating
     mutating func choose(card: Card) {
-        let tappedCardIndex: Int = self.index(of: card)
+        let tappedCardIndex: Int = cards.firstIndex(matching: card)
         print("User chose card: \(card) and tappedCardIndex is: \(tappedCardIndex)")
         cards[tappedCardIndex].isFaceUp = !cards[tappedCardIndex].isFaceUp
     }
     
-    func index(of tappedCard: Card) -> Int {
-        for card in cards {
-            if card.id == tappedCard.id {
-                return tappedCard.id
-            }
-        }
-        return 0  // FIXME: - Bogus
-    }
+//    func index(of tappedCard: Card) -> Int {
+//        for card in cards {
+//            if card.id == tappedCard.id {
+//                return tappedCard.id
+//            }
+//        }
+//        return 0  // FIXME: - Bogus
+//    }
     
     struct Card: Identifiable {
         var isFaceUp: Bool = true
