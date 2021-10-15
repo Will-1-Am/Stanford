f60799228c8f5cfa1b42530b05376aa9a64ebd2f
William Spanfelner <wil-1-am@outlook.com>
2021-10-15 21:07:00 +0300

    Task 3 Architect the concept of a “theme” into your game. A theme consists of a name for the theme, a set of emoji to use, a number of cards to show (which, for at least one, but not all themes, should be random), and an appropriate color to use to draw (e.g. orange would be appropriate for a Halloween theme).

--- MemorizeCS193p/ViewModel/EmojiMemoryGame.swift
+++ MemorizeCS193p/ViewModel/EmojiMemoryGame.swift
@@ -8,14 +8,30 @@
 import Foundation
 
 class EmojiMemoryGame: ObservableObject {
-    @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    @Published private var model: MemoryGame<String>
     
-    static func createMemoryGame() -> MemoryGame<String> {
-        let emojis: Array<String> = ["👻", "🎃", "🎭"]
-        return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
+    // MARK: - The theme property should be visible by the UI
+    private(set) var theme: Theme
+    
+    // MARK: - Initialise the theme and model properties - force unwrapping the optional first element as this will certainly exist. Utilize the newly created argument for createMemoryGame
+    init() {
+        self.theme = EmojiMemoryGame.themes.first!
+        self.model = EmojiMemoryGame.createMemoryGame(using: theme)
+    }
+    
+    // MARK: - add a new argument to the createMemoryGame function to incorporate the theme into the game creation process
+    static func createMemoryGame(using theme: Theme) -> MemoryGame<String> {
+        let emojis: Array<String> = theme.setOfEmoji
+        let randomNumberOfPairs = Int.random(in: 2 ... emojis.count)
+        return MemoryGame<String>(numberOfPairsOfCards: theme.numberOfPairsToShow ?? randomNumberOfPairs) { pairIndex in
             return emojis[pairIndex]
         }
     }
+    
+    // MARK: - Setup a basic array of themes for the type
+    static var themes: Array<Theme> = [
+        Theme()
+    ]
     
     // MARK: - Access to the model
     var cards: Array<MemoryGame<String>.Card> {
---
+++ MemorizeCS193p/ViewModel/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  MemorizeCS193p
+//
+//  Created by William Spanfelner on 15/10/2021.
+//
+
+import SwiftUI
+
+struct Theme {
+    var name: String = "Halloween"
+    var setOfEmoji: [String] = ["👻", "🎃", "🎭"]
+    var numberOfPairsToShow: Int? = 3
+    var themeColor: Color = .orange
+}
