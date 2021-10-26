e1b230a5007a7316a8c3b4b412f2a41a3a4a31bd
William Spanfelner <wil-1-am@outlook.com>
2021-10-26 21:47:54 +0300

    Use Cardify by factoring redundant code from View;

--- MemorizeCS193p/EmojiMemoryGameView.swift
+++ MemorizeCS193p/EmojiMemoryGameView.swift
@@ -33,27 +33,22 @@ struct CardView: View {
     
     private func body(for size: CGSize) -> some View {
         ZStack {
-            if card.isFaceUp {
-                self.front(of: card)
-            } else if card.isMatched {
-                RoundedRectangle(cornerRadius: cornerRadius)
-                    .opacity(matchedCardOpacity)
-            } else {
-                RoundedRectangle(cornerRadius: cornerRadius)
-            }
-        }
-        .font(.system(size: fontSize(for: size)))
-    }
-    
-    @ViewBuilder
-    private func front(of card: MemoryGame<String>.Card) -> some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
-            RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
             Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 70 - 90), clockwise: true).padding(5).opacity(0.3)
             Text(card.content)
         }
+        .modifier(Cardify(isFaceUp: card.isFaceUp))
+        .font(.system(size: fontSize(for: size)))
     }
+    
+//    @ViewBuilder
+//    private func front(of card: MemoryGame<String>.Card) -> some View {
+//        ZStack {
+//            RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+//            RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+//            Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 70 - 90), clockwise: true).padding(5).opacity(0.3)
+//            Text(card.content)
+//        }
+//    }
     
     private func fontSize(for size: CGSize) -> CGFloat {
         return .minimum(size.width, size.height) * fontScaleFactor
