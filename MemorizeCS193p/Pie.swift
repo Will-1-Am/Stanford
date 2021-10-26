b3b6764d0616f12f4e4e5be72702befaa5b9968f
William Spanfelner <wil-1-am@outlook.com>
2021-10-26 20:50:31 +0300

    Add a line back to the center to complete the pie

--- MemorizeCS193p/Pie.swift
+++ MemorizeCS193p/Pie.swift
@@ -10,20 +10,19 @@ import SwiftUI
 struct Pie: Shape {
     var startAngle: Angle
     var endAngle: Angle
-    // MARK: - Should the arc be drawn clockwise?
     var clockwise: Bool = false
 
     func path(in rect: CGRect) -> Path {
         let center = CGPoint(x: rect.midX, y: rect.midY)
-        // MARK: - Compute the radius
         let radius = .minimum(rect.width, rect.height) / 2
         let start = CGPoint(x: center.x + radius * cos(CGFloat(startAngle.radians)), y: center.y + radius * sin(CGFloat(startAngle.radians)))
         var p = Path()
         p.move(to: center)
         p.addLine(to: start)
-        
-        // MARK: - Add an arc using the values of the properties already defined
         p.addArc(center: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: clockwise)
+        
+        // MARK: - Add a line back to the center to complete the pie
+        p.addLine(to: center)
         
         return p
     }
