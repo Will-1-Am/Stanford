d7ef0cf117d4c39b189e262ecd6ff7971af64ed9
William Spanfelner <wil-1-am@outlook.com>
2021-10-28 21:34:56 +0300

    Add the repeatForever() function to the animation;

--- MemorizeCS193p/EmojiMemoryGameView.swift
+++ MemorizeCS193p/EmojiMemoryGameView.swift
@@ -35,10 +35,7 @@ struct CardView: View {
         ZStack {
             Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 70 - 90), clockwise: true).padding(5).opacity(0.3)
             Text(card.content).rotationEffect(Angle(degrees: card.isMatched ? 360 : 0))
-            // MARK: - Prior to iOS15 the animation modifier could be written like so
-//                .animation(Animation.linear(duration: 3))
-            // MARK: - iOS15 has deprecated the above and now must be written something like the following
-                .animation(Animation.linear(duration: 3), value: card.isMatched)
+                .animation(Animation.linear(duration: 3).repeatForever(), value: card.isMatched)
         }
         .cardify(card)
         .font(.system(size: fontSize(for: size)))
