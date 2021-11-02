d3663cbc7ee45d3cab8d655a03121841c414fe27
William Spanfelner <wil-1-am@outlook.com>
2021-11-02 21:03:22 +0200

    Add autoreverses argument to repeatForever function;

--- MemorizeCS193p/EmojiMemoryGameView.swift
+++ MemorizeCS193p/EmojiMemoryGameView.swift
@@ -35,7 +35,7 @@ struct CardView: View {
         ZStack {
             Pie(startAngle: Angle(degrees: 0 - 90), endAngle: Angle(degrees: 70 - 90), clockwise: true).padding(5).opacity(0.3)
             Text(card.content).rotationEffect(Angle(degrees: card.isMatched ? 360 : 0))
-                .animation(Animation.linear(duration: 3).repeatForever(), value: card.isMatched)
+                .animation(Animation.linear(duration: 3).repeatForever(autoreverses: true), value: card.isMatched)
         }
         .cardify(card)
         .font(.system(size: fontSize(for: size)))
